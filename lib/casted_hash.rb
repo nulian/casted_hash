219e4e89e651e11ad23dc3a154bdd83131aaeafc
Stephan Kaag <stephan@ka.ag>
2015-06-10 11:47:15 +0200

    Bump version

--- lib/casted_hash.rb
+++ lib/casted_hash.rb
@@ -1,5 +1,5 @@
 class CastedHash < Hash
-  VERSION = "0.6.0"
+  VERSION = "0.7.0"
 
   def initialize(constructor = {}, cast_proc = nil)
     raise ArgumentError, "`cast_proc` required" unless cast_proc
